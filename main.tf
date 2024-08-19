91a70a4149690c3faa05cc9cc918303b3731a2ef
Pablo Maman <pvdemaman@gmail.com>
2024-08-18 23:59:38 -0300

    main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 # main.tf
 
-
-provider "azurerm" {
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
 }
 
 module "net" {
